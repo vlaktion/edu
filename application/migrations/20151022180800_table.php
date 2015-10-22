fb57a9510ae2ed530eefb94590832691e5aa6187
Vlad <vlad.progers@gmail.com>
2015-10-22 18:09:59 +0300

    Создал таблицу pages33

---
+++ application/migrations/20151022180800_table.php
@@ -0,0 +1,55 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Some_table extends CI_Migration {
+
+	public function up()
+	{
+		// Drop table 'pages' if it exists
+		$this->dbforge->drop_table('pages33', TRUE);
+
+		// Table structure for table 'pages'
+		$this->dbforge->add_field(array(
+			'id' => array(
+				'type' => 'MEDIUMINT',
+				'constraint' => '8',
+				'unsigned' => TRUE,
+				'auto_increment' => TRUE
+			),
+			'title' => array(
+				'type' => 'VARCHAR',
+				'constraint' => '50',
+			),
+			'description' => array(
+				'type' => 'VARCHAR',
+				'constraint' => '200',
+			),
+			'keywords' => array(
+				'type' => 'VARCHAR',
+				'constraint' => '50',
+			),
+			'content' => array(
+				'type' => 'TEXT',
+				'null' => TRUE,
+			),
+		));
+		$this->dbforge->add_key('id', TRUE);
+		$this->dbforge->create_table('pages33');
+
+		// Dumping data for table 'pages'
+		$data = array(
+			array(
+				'id' => '1',
+				'title' => 'Главная страница',
+				'description' => 'Описание',
+				'keywords' => '',
+				'content' => 'Текст',
+			),
+		);
+		$this->db->insert_batch('pages33', $data);
+	}
+
+	public function down()
+	{
+		$this->dbforge->drop_table('pages33', TRUE);
+	}
+}
--- index.php
+++ index.php
@@ -63,6 +63,7 @@
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
+print_r(apache_get_modules());
 switch (ENVIRONMENT)
 {
 	case 'development':
