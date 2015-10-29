1a5415c420d4c84951b2605e455fdd36c5622994
Eugene Melnik <sike1@yandex.ru>
2015-10-29 17:44:44 +0200

    change_password_view edit

--- application/views/auth/change_password.php
+++ application/views/auth/change_password.php
@@ -1,25 +1,32 @@
-<h1><?php echo lang('change_password_heading');?></h1>
 
-<div id="infoMessage"><?php echo $message;?></div>
 
-<?php echo form_open("auth/change_password");?>
-
-      <p>
-            <?php echo lang('change_password_old_password_label', 'old_password');?> <br />
-            <?php echo form_input($old_password);?>
-      </p>
-
-      <p>
-            <label for="new_password"><?php echo sprintf(lang('change_password_new_password_label'), $min_password_length);?></label> <br />
-            <?php echo form_input($new_password);?>
-      </p>
-
-      <p>
-            <?php echo lang('change_password_new_password_confirm_label', 'new_password_confirm');?> <br />
+<div class="col-md-6">
+  <!-- general form elements -->
+  <div class="box box-primary">
+    <div class="box-header with-border">
+      <h3 class="box-title"><?php echo lang('change_password_heading');?></h3>
+      <div id="infoMessage"><?php echo $message;?></div>
+    </div><!-- /.box-header -->
+    <!-- form start -->
+    <?php echo form_open("auth/change_password");?>
+      <div class="box-body">
+        <div class="form-group">
+          <?php echo lang('change_password_old_password_label', 'old_password');?> <br />
+          <?php echo form_input($old_password);?>
+        </div>
+        <div class="form-group">
+          <label for="new_password"><?php echo sprintf(lang('change_password_new_password_label'), $min_password_length);?></label> <br />
+          <?php echo form_input($new_password);?>
+        </div>
+        <div class="form-group">
+          <?php echo lang('change_password_new_password_confirm_label', 'new_password_confirm');?> <br />
             <?php echo form_input($new_password_confirm);?>
-      </p>
-
-      <?php echo form_input($user_id);?>
-      <p><?php echo form_submit('submit', lang('change_password_submit_btn'));?></p>
-
-<?php echo form_close();?>
+        </div>
+      </div><!-- /.box-body -->
+      <div class="box-footer">
+        <?php echo form_input($user_id);?>
+        <?php echo form_submit('submit', lang('change_password_submit_btn'));?>
+      </div>
+    <?php echo form_close();?>
+  </div><!-- /.box -->
+</div>
