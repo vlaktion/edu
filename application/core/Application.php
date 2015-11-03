63c4179f9c803b9b4bca45bc99a3bc9875e9f181
Vlad <vlad.progers@gmail.com>
2015-11-03 17:03:30 +0200

    Починил Forfot Password

--- application/core/Application.php
+++ application/core/Application.php
@@ -8,10 +8,12 @@ class Application extends MY_Controller
         parent::__construct();
         $this->load->library(array('ion_auth','form_validation'));
 
-    	if (!$this->ion_auth->logged_in() && ($this->uri->segment($this->uri->total_segments()) != 'login'))
-		{
-			// redirect them to the login page
-			redirect('auth/login', 'refresh');
+    	if (!$this->ion_auth->logged_in()){
+    		if(($this->uri->segment($this->uri->total_segments()) != 'login')&&($this->uri->segment($this->uri->total_segments()) != 'forgot_password'))
+			{
+				// redirect them to the login page
+				redirect('auth/login', 'refresh');
+			}
 		}
 
 	}
