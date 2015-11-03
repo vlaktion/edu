6d772b04a3d10e5ed7bf9093e195f5d2c49f849e
mihailsadovniko <mihail_sadovniko@mail.ru>
2015-11-03 17:21:59 +0200

    Создание групп верстка

--- application/views/auth/create_group.php
+++ application/views/auth/create_group.php
@@ -9,11 +9,11 @@
 <?php echo form_open("auth/create_group");?>
 
       <p>
-            <?php echo lang('create_group_name_label', 'group_name');?><?php echo form_input($group_name);?>
+            <?php echo lang('create_group_name_label', 'group_name');?> <?php echo form_input($group_name);?>
       </p>
 
       <p>
-            <?php echo lang('create_group_desc_label', 'description');?><?php echo form_input($description);?>
+            <?php echo lang('create_group_desc_label', 'description');?> <?php echo form_input($description);?>
       </p>
 
       <p><?php echo form_submit('submit', lang('create_group_submit_btn'));?></p>
